383d5c49bc9ca52597ec32c03868e70eb9efea80
Ambaliya Dhruv <ambaliyadhruv1112@gmail.com>
2022-05-07 16:51:07 +0530

    how and where to use map and way of write code

--- HOF.js
+++ HOF.js
@@ -68,9 +68,9 @@ function diameter(radius){
 
 // original map function is 
 
-console.log(radius.map(area));
+// console.log(radius.map(area));
 
-console.log(BluePrint(radius,area));
+// console.log(BluePrint(radius,area));
 
 // above code is give me same output but not look like similar 
 
@@ -84,6 +84,48 @@ Array.prototype.BluePrint=function(logic){
     return answer ; 
 }
 
-console.log(radius.BluePrint(area));
+// console.log(radius.BluePrint(area));
 
 // now we can use this code as map function in our code 
+
+
+// let understand how to use map and where to use iton code 
+
+// let one array is given and we want to transformation to this array to we use map 
+
+// ex= let arr=[1,2,3,4,5,6]  and I want to transform to [2,4,6,8,10,12] this type array so I used map function
+
+// let Arraymap=[1,2,3,4,5,6]; 
+
+// console.log(Arraymap.map(item=>item*2));
+
+// also i convert into binary number
+// way of write map code
+// normal anomyous function
+
+
+// console.log(Arraymap.map(function(item){
+//     return item.toString(2);
+// }));
+
+
+// arrow function
+
+
+// console.log(Arraymap.map((item)=>{
+//     return item.toString(2);
+// }))
+
+// without return statement beacuse only one line of code
+
+// console.log(Arraymap.map((item)=>item.toString(2)));
+
+// function declaration sperate from function expression
+
+// function binary(item){
+//     return item.toString(2);
+// }
+
+// console.log(Arraymap.map(binary));
+
+// this is usecase of map function
